0489a4fa3c393d54c24a610f1bf9293da331061b
Chris Neale <cin210@nyu.edu>
2013-03-09 20:42:51 -0800

    Ch-ch-ch-changes

--- scripts/html.ts
+++ scripts/html.ts
@@ -11,11 +11,41 @@ $("/html") {
   @import sections/footer.ts
 
   @import mappings.ts
+	
+	
+	
 	$$("#main"){
     move_to('../..', 'top');
+
+		inject_top('<div id="navi">
+		  <a href="#main" class="nav_button">Top</a>
+		  <a href="#sponsors" class="nav_button">Bot</a>
+			</div>');
+	}
+	
+	$$(".primary-links"){
+	  move_to('../..', 'top');
+	}
+	$$(".notice"){
+	  move_to('../../../../..', 'top')
+	}
+
+	$$("#top"){
+	  move_to('../..', 'top');
+	}
+	
+	$$(".shortcuts"){
+	  remove()
 	}
 	$$("#sponsors") { 
     move_to('..', 'bottom');
+	}
+	$$("a.logo"){
+	  move_to("../../..", "top")
+ 	}
+	$$('#search'){
+
+	inject_after('<div id="moovweb_plug">Transformed in real time by <a href="http://www.moovweb.com">Moovweb</a></div>');
 	}
 }
 
