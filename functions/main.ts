fbf63b42bc4cf162021d3ae78fa2fa6952dceedd
Chris Neale <cin210@nyu.edu>
2013-03-09 21:12:38 -0800

    More changes (aren't these so detailed?)

--- functions/main.ts
+++ functions/main.ts
@@ -270,7 +270,40 @@
 
 @func XMLNode.strip() {
   $('./body'){
-	  
+	
+		$$("#main"){
+	    move_to('../..', 'top');
+
+			inject_top('<div id="navi">
+			  <a href="#main" class="nav_button">Top</a>
+			  <a href="#sponsors" class="nav_button">Bot</a>
+				</div>');
+		}
+	
+		$$(".primary-links"){
+		  move_to('../..', 'top');
+		}
+		$$(".notice"){
+		  move_to('../../../../..', 'top')
+		}
+
+		$$("#top"){
+			remove()
+		}
+	
+		$$(".shortcuts"){
+		  remove()
+		}
+		$$("#sponsors") { 
+	    move_to('..', 'bottom');
+		}
+		$$("a.logo"){
+		  move_to("../../..", "top")
+	 	}
+		$$('#search'){
+
+		inject_after('<div id="moovweb_plug">Transformed in real time by <a href="http://www.moovweb.com">Moovweb</a></div>');
+		}
 	}
 }
 
